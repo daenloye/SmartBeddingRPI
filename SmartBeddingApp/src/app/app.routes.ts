b0eafd01f848dfc91141c62f28a0d3c85b2071dd
daenloye <ldanielenrique@gmail.com>
2026-02-27 08:58:54 -0500

    Se inicializa el componente de conectividad

--- SmartBeddingApp/src/app/app.routes.ts
+++ SmartBeddingApp/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TemplatePanelComponent } from './panel/template-panel/template-panel.component';
 import { HomeComponent } from './panel/home/home.component';
+import { ConnectivityComponent } from './panel/connectivity/connectivity.component';
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
@@ -10,6 +11,7 @@ export const routes: Routes = [
     component: TemplatePanelComponent,
     children:[
       {path: '', component: HomeComponent},
+      {path: 'connectivity', component: ConnectivityComponent},
     ]
 
   }
---
+++ SmartBeddingApp/src/app/panel/connectivity/connectivity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connectivity',
+  imports: [],
+  templateUrl: './connectivity.component.html',
+  styleUrl: './connectivity.component.css'
+})
+export class ConnectivityComponent {
+
+}
